4b913aa5ed47c30ce256ac0ac733f0b19749a32d
Keryann Malacchina <keryannm@gmail.com>
2017-01-16 16:56:04 +0100

    working on anim

--- Assets/Scripts/IsaacAnim.cs
+++ Assets/Scripts/IsaacAnim.cs
@@ -21,6 +21,7 @@ public class IsaacAnim : MonoBehaviour
 	void Start () 
 	{
 		isaacFire = GetComponent<IsaacFire> ();
+
 		StartCoroutine (OnMovementChange (movementState));
 		StartCoroutine (OnFireChange (fireState));
 	}
@@ -30,29 +31,7 @@ public class IsaacAnim : MonoBehaviour
 	{
 		GetFireDirection ();
 
-		GetMovement ();
-	}
-
-	void GetMovement ()
-	{
-		if (Input.GetAxisRaw ("Horizontal") > 0)
-			movementState = MovementState.Right;
-
-		else if (Input.GetAxisRaw ("Horizontal") < 0)
-			movementState = MovementState.Left;
-
-		else if(Input.GetAxisRaw ("Horizontal") == 0 && Input.GetAxisRaw ("Vertical") != 0)
-		{
-			if (Input.GetAxisRaw ("Vertical") > 0)
-				movementState = MovementState.Up;
-
-			if (Input.GetAxisRaw ("Vertical") < 0)
-				movementState = MovementState.Down;
-		}
-
-		else if(Input.GetAxisRaw ("Horizontal") == 0 && Input.GetAxisRaw ("Vertical") == 0)
-			movementState = MovementState.Idle;
-
+		GetMovementDirection ();
 	}
 
 	void GetFireDirection ()
@@ -75,11 +54,65 @@ public class IsaacAnim : MonoBehaviour
 			fireState = FireState.Idle;
 	}
 
+	void GetMovementDirection ()
+	{
+		if (Input.GetAxisRaw ("Horizontal") > 0)
+			movementState = MovementState.Right;
+
+		else if (Input.GetAxisRaw ("Horizontal") < 0)
+			movementState = MovementState.Left;
+
+		else if(Input.GetAxisRaw ("Horizontal") == 0 && Input.GetAxisRaw ("Vertical") != 0)
+		{
+			if (Input.GetAxisRaw ("Vertical") > 0)
+				movementState = MovementState.Up;
+
+			if (Input.GetAxisRaw ("Vertical") < 0)
+				movementState = MovementState.Down;
+		}
+
+		else if(Input.GetAxisRaw ("Horizontal") == 0 && Input.GetAxisRaw ("Vertical") == 0)
+			movementState = MovementState.Idle;
+
+	}
+
+
+	void SendXMovement ()
+	{
+		if (movementState == MovementState.Right)
+		{
+			bodyRend.flipX = false;
+			bodyAnimator.SetBool ("xMovement", true);
+		}
+
+		else if(movementState == MovementState.Left)
+		{
+			bodyRend.flipX = true;
+			bodyAnimator.SetBool ("xMovement", true);
+		}
+
+		else
+		{
+			bodyRend.flipX = false;
+			bodyAnimator.SetBool ("xMovement", false);
+		}
+
+	}
+
+	void SendYMovement ()
+	{
+		if(movementState == MovementState.Up || movementState == MovementState.Down)
+			bodyAnimator.SetBool ("yMovement", true);
+
+		else
+			bodyAnimator.SetBool ("yMovement", false);
+	}
+
+
 	IEnumerator OnMovementChange (MovementState previousState)
 	{
-		SetHead ();
-		
-		SetBody ();
+		SendXMovement ();
+		SendYMovement ();
 
 		yield return new WaitUntil (() => previousState != movementState);
 
@@ -94,6 +127,7 @@ public class IsaacAnim : MonoBehaviour
 
 		StartCoroutine (OnFireChange (fireState));
 	}
+
 
 	void SetHead ()
 	{
@@ -146,21 +180,21 @@ public class IsaacAnim : MonoBehaviour
 		switch(movementState)
 		{
 		case MovementState.Down:
-			bodyAnimator.SetTrigger ("Body_UpDown");
+			//bodyAnimator.SetTrigger ("Body_UpDown");
 			break;
 		case MovementState.Up:
-			bodyAnimator.SetTrigger ("Body_UpDown");
+			//bodyAnimator.SetTrigger ("Body_UpDown");
 			break;
 		case MovementState.Right:
-			bodyAnimator.SetTrigger ("Body_LeftRight");
+			//bodyAnimator.SetTrigger ("Body_LeftRight");
 			bodyRend.flipX = false;
 			break;
 		case MovementState.Left:
-			bodyAnimator.SetTrigger ("Body_LeftRight");
+			//bodyAnimator.SetTrigger ("Body_LeftRight");
 			bodyRend.flipX = true;
 			break;
 		case MovementState.Idle:
-			bodyAnimator.SetTrigger ("Body_Idle");
+			//bodyAnimator.SetTrigger ("Body_Idle");
 			bodyRend.flipX = false;
 			bodyRend.flipY = false;
 			break;
